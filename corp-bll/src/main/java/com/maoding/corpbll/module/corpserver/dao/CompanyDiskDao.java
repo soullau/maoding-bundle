a673a310f75140cff37ad5c105a926474cc27dc0
svn <svn@imaoding.com>
2017-06-06 17:12:32 +0800

    从svn更新

---
+++ corp-bll/src/main/java/com/maoding/corpbll/module/corpserver/dao/CompanyDiskDao.java
@@ -0,0 +1,10 @@
+package com.maoding.corpbll.module.corpserver.dao;
+
+import com.maoding.core.base.BaseDao;
+import com.maoding.corpbll.module.corpserver.model.CompanyDiskDo;
+
+/**
+ * Created by Wuwq on 2017/06/05.
+ */
+public interface CompanyDiskDao extends BaseDao<CompanyDiskDo> {
+}
