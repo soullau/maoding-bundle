27e43b051f5fdc96e619ba2a34e6ed016da5aa9f
zhangchengliang <zhangchengliang@imaoding.com>
2017-07-19 18:47:56 +0800

    实现controller接口

--- web-admin-bll/src/main/java/com/maoding/admin/module/historyData/service/ImportServiceImpl.java
+++ web-admin-bll/src/main/java/com/maoding/admin/module/historyData/service/ImportServiceImpl.java
@@ -3,7 +3,10 @@ package com.maoding.admin.module.historyData.service;
 import com.maoding.admin.module.historyData.dto.ProjectImportDTO;
 import com.maoding.core.base.BaseService;
 import com.maoding.core.bean.ApiRequestInsert;
+import com.maoding.utils.ExcelUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Chengliang.zhang on 2017/7/19.
@@ -17,6 +20,7 @@ public class ImportServiceImpl extends BaseService implements ImportService {
      */
     @Override
     public void importProjects(ApiRequestInsert<ProjectImportDTO> request) {
-
+        if ((request == null) || (request.getData() == null)) throw new IllegalArgumentException("importProjects 参数错误");
+        List<List<Object>> dataList = ExcelUtils.readFrom(request.getData().getImportFile(),-1,4,6,null,null);
     }
 }
