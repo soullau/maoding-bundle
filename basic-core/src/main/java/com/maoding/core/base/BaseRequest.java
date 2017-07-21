a2f0b3987aecf2d54a3588ca587eccf0e89b8188
zhangchengliang <zhangchengliang@imaoding.com>
2017-07-21 15:54:04 +0800

    更改ApiRequest说明

--- basic-core/src/main/java/com/maoding/core/base/BaseRequest.java
+++ basic-core/src/main/java/com/maoding/core/base/BaseRequest.java
@@ -4,48 +4,17 @@ import java.io.Serializable;
 
 /**
  * Created by Chengliang.zhang on 2017/7/18.
- * 操作请求基类，后续可
+ * 操作请求基类
  */
 public class BaseRequest implements Serializable {
-    /** 操作者标识 */
-    AuthToken token;
+    /** 操作者标识，目前使用String类型，以后可转换为token类型 */
+    String token;
 
-    /** 模拟token类型 */
-    public class AuthToken {
-        private String account;
-        private String password;
-        private String token;
-
-        public String getAccount() {
-            return account;
-        }
-
-        public void setAccount(String account) {
-            this.account = account;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getToken() {
-            return token;
-        }
-
-        public void setToken(String token) {
-            this.token = token;
-        }
-    }
-
-    public AuthToken getToken() {
+    public String getToken() {
         return token;
     }
 
-    public void setToken(AuthToken token) {
+    public void setToken(String token) {
         this.token = token;
     }
 }
--- basic-core/src/main/java/com/maoding/core/bean/ApiRequest.java
+++ basic-core/src/main/java/com/maoding/core/bean/ApiRequest.java
@@ -9,17 +9,19 @@ import java.util.List;
  * 请求操作数据时用到的参数类型
  */
 public final class ApiRequest<T> extends BaseRequest {
-    /** 目标数据
-     * 除id字段的其他字段为null则不操作，为0或""记录保存为指定值
+    /**
+     * 目标数据，除id字段的其他字段为null则不操作，为0或""记录保存为指定值
      */
     T data;
-    /** 要更改或删除的记录的id，可以是多个
+    /**
+     * 要更改或删除的记录的id，可以是多个
      */
     List<String> idList;
 
     /* 如果data内id字段为空，并且idList为空，则属于添加数据操作申请 */
     /* 如果data为空，并且idList不为空，则属于删除数据操作申请 */
     /* 如果data不为空，并且data内id不为空或idList不为空，则属于更改数据操作申请 */
+    /* data内id不为空时，如果idList为空则只操作id指定的数据，如果idList不为空，则忽略id */
 
     public T getData() {
         return data;
